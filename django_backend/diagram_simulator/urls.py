ffbe82d5307e4646d7f7886113e2303e7daf05a8
Mohamed Abdelwahab <elsayeedm@gmail.com>
2025-08-20 01:59:26 -0700

    Add django_backend/diagram_simulator/urls.py 1755680364655

---
+++ django_backend/diagram_simulator/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for diagram_simulator project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('simulator.urls')),
+]
